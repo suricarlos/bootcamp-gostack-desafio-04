ffa8a17a5168e1cf4848b7824f9a32dd960d21d1
Your Name <carlao.linux@gmail.com>
2019-11-23 00:07:27 -0300

    update

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Header from './components/Header';
 class App extends Component {
   render() {
     return (
-      <>
+      <div className="App">
         <Header />
-      </>
+      </div>
     )
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import '../App.css';
+// import '../App.css';
 
 function Header() {
   return(
-    <div className="logo">
-      Hello
+    <div className="header">
+      <img src={require('../assets/facebook.svg')} alt="logo facebook" />
+      <div>
+        <p>Meu perfil</p>
+        <img src={require('../assets/icon_login.png')} alt="icon login" />
+      </div>
     </div>
   )
 }
